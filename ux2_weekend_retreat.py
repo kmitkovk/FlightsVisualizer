b5f414986e7b89a1f8bda75f15c685a890186e8b
Krasimir Mitkov Kerliev <krasimirk@gen-i.si>
2022-08-08 00:51:59 +0200

    UX2 visual draft version and logic

--- ux2_weekend_retreat.py
+++ ux2_weekend_retreat.py
@@ -32,19 +32,34 @@ deps = df[df.flight.str.startswith("ZAG")].drop(["timestamp"], axis=1)
 arrs = df[df.flight.str.startswith("EIN")].drop(["timestamp"], axis=1)
 
 #%% Extract:
-days_diff_max = 20
+days_diff_max = 5
 days_diff_min = 1
 days_delta_max = dt.timedelta(days=days_diff_max)
 days_delta_min = dt.timedelta(days=days_diff_min)
 
 df_matching = pd.DataFrame()
-for dep in deps.iterrows():
+for c, dep in enumerate(deps.iterrows()):
+    origin = dep[1].flight[:3]
     origin_dep_date = dep[1].departure_date
     origin_dep_price = dep[1].price
     arr_dep_delta = arrs.departure_date - origin_dep_date
     matching_return_flights = arrs[
         (arr_dep_delta <= days_delta_max) & (arr_dep_delta >= days_delta_min)
     ]
-    matching_return_flights["d_delta"] = (
+    matching_return_flights.loc[:, "d_delta"] = (
         matching_return_flights.departure_date - origin_dep_date
     )
+    matching_return_flights.loc[:, "total_fare"] = (
+        matching_return_flights.price + origin_dep_price
+    )
+
+    matching_return_flights["outbound_date"] = origin_dep_date
+    # matching_return_flights["id_trip"] = str(c) + "_" + origin + "_"
+
+    df_matching = pd.concat([df_matching, matching_return_flights])
+
+df_matching = df_matching.sort_values('outbound_date').reset_index()
+df_matching['flight'] = df_matching.index.astype('str') + "_" + df_matching.flight
+df_matching.departure_date = df_matching.departure_date.dt.strftime('%Y-%m-%d')
+df_matching.outbound_date = df_matching.outbound_date.dt.strftime('%Y-%m-%d')
+df_matching.d_delta = df_matching.d_delta.dt.days
--- visuals/app_dash/src/pages/ux2.py
+++ visuals/app_dash/src/pages/ux2.py
@@ -5,7 +5,6 @@ import dash
 import dash_bootstrap_components as dbc
 
 import plotly.express as px
-import plotly.figure_factory as ff
 from dash import Input, Output, dcc, html
 
 
@@ -51,44 +50,36 @@ layout = dbc.Container(
 
 @dash.callback(Output("s-chart", "figure"), Input("s-dd", "value"))
 def update_output(countries):
-
     # Source: https://plotly.com/python/gantt/
-    df = pd.DataFrame(
-        [
-            dict(
-                Task="Job A", Start="2021-01-08", Finish="2021-01-29", Completion_pct=50
-            ),
-            dict(
-                Task="Job B", Start="2021-01-15", Finish="2021-02-12", Completion_pct=60
-            ),
-            dict(
-                Task="Job C", Start="2021-01-29", Finish="2021-02-19", Completion_pct=70
-            ),
-            dict(
-                Task="Job D", Start="2021-02-05", Finish="2021-02-26", Completion_pct=80
-            ),
-        ]
-    )
+    # Source: https://plotly.com/python-api-reference/generated/plotly.express.timeline.html
+    # Colors: https://plotly.com/python/builtin-colorscales/
+    # Colors: https://plotly.com/python/colorscales/
+
+    data2 = {'flight': {1506: 'EIN_ZAG1', 1507: 'EIN_ZAG2', 1508: 'EIN_ZAG3', 1509: 'EIN_ZAG4', 1502: 'EIN_ZAG5', 1503: 'EIN_ZAG6', 1504: 'EIN_ZAG7', 1505: 'EIN_ZAG8', 1510: 'EIN_ZAG9'}, 'departure_date': {1506: '2022-09-02', 1507: '2022-09-05', 1508: '2022-09-09', 1509: '2022-09-12', 1502: '2022-09-16', 1503: '2022-09-19', 1504: '2022-09-23', 1505: '2022-09-26', 1510: '2022-09-30'}, 'price': {1506: 57.0, 1507: 20.0, 1508: 24.0, 1509: 24.0, 1502: 43.0, 1503: 23.0, 1504: 27.0, 1505: 18.0, 1510: 22.0}, 'd_delta': {1506: 4, 1507: 3, 1508: 4, 1509: 3, 1502: 4, 1503: 3, 1504: 4, 1505: 3, 1510: 4}, 'total_fare': {1506: 150.0, 1507: 92.0, 1508: 104.0, 1509: 76.0, 1502: 96.0, 1503: 82.0, 1504: 105.0, 1505: 75.0, 1510: 56.0}, 'outbound_date': {1506: '2022-08-29', 1507: '2022-09-02', 1508: '2022-09-05', 1509: '2022-09-09', 1502: '2022-09-12', 1503: '2022-09-16', 1504: '2022-09-19', 1505: '2022-09-23', 1510: '2022-09-26'}}
+    df = pd.DataFrame(data2)
+    df["custom_name"] = df.apply(lambda row: "€" + str(row.total_fare), axis=1)
+
+    dates = pd.date_range(df.outbound_date.min(), df.departure_date.max())
 
     fig = px.timeline(
-        df, x_start="Start", x_end="Finish", y="Task", color="Completion_pct"
+        df,
+        x_start="outbound_date",
+        x_end="departure_date",
+        y="flight",
+        text="custom_name",
+        color="total_fare",
+        color_continuous_scale=px.colors.diverging.Temps,
+        title = 'Flight-combos:',
+        # width=1600, height=800)
     )
 
-    # my_figure = ff.create_gantt(df, showgrid_x=True, showgrid_y=True)
-    # weeks_ticktext = ["KW: 01 ('21)", "", "", "", "KW: 05 ('21)", "", "", "", ""]
-    weeks_tickvals = [
-        datetime.datetime(2021, 1, 4, 0, 0),
-        datetime.datetime(2021, 1, 11, 0, 0),
-        datetime.datetime(2021, 1, 18, 0, 0),
-        datetime.datetime(2021, 1, 25, 0, 0),
-        datetime.datetime(2021, 2, 1, 0, 0),
-        datetime.datetime(2021, 2, 8, 0, 0),
-        datetime.datetime(2021, 2, 15, 0, 0),
-        datetime.datetime(2021, 2, 22, 0, 0),
-        datetime.datetime(2021, 3, 1, 0, 0),
-    ]
-    fig.layout.xaxis["tickmode"] = "array"
-    fig.layout.xaxis["tickvals"] = weeks_tickvals
-    fig.layout.xaxis["ticktext"] = weeks_ticktext
+    fig.update_yaxes(autorange="reversed")
+    for date in dates[dates.day_of_week.isin([5, 6])]:
+        fig.add_vline(x=date, line_width=2, line_color="gray", opacity=0.3)
+    for date in dates[dates.day_of_week.isin([0, 1, 2, 3, 4])]:
+        fig.add_vline(
+            x=date, line_width=1, line_color="gray", line_dash="dash", opacity=0.2
+        )
+    fig.update_layout(plot_bgcolor="rgba(0,0,0,0)")
 
     return fig
